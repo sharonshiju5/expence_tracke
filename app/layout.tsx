da95e249ccf8ab4e034ac015375b8784f9669ff8
sharonshiju5 <sharonshiju261@gmail.com>
2025-11-28 11:57:31 +0530

    font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import HomeWrapper from "@/components/HomeWrapper";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const poppins = Poppins({
+  variable: "--font-poppins",
   subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
+ 
 export const metadata: Metadata = {
   title: "Smart Ledger & Expense Tracker",
   description: "A comprehensive expense tracking and ledger management system",
@@ -26,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black`}
+        className={`${poppins.variable} antialiased bg-black font-poppins`}
         suppressHydrationWarning={true}
       >
         <HomeWrapper>{children}</HomeWrapper>
--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,10 @@ const Header = () => {
   };
 
   return (
-    <div className='fixed top-0 left-0 right-0 bg-black z-50 p-4 flex justify-end'>
+    <div className='fixed top-0 left-0 right-0 bg-black z-50 p-4  flex justify-between items-center'>
+      <div className='flex items-center mr-4'>
+        <h1 className='text-white text-xl ml-'>Expense Tracker</h1>
+      </div>
       <button 
         onClick={handleLogout}
         className='bg-red-500 text-white px-4 py-2 rounded-lg text-sm font-medium hover:bg-red-600'
