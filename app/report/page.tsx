b499bcc0d55318623d94f58c95b41047dac3ded4
sharonshiju5 <sharonshiju261@gmail.com>
2025-11-29 12:31:49 +0530

    styling

--- app/report/page.tsx
+++ app/report/page.tsx
@@ -51,7 +51,7 @@ const ReportPage = () => {
   }
 
   return (
-    <div className='bg-black min-h-screen p-4 sm:p-6 lg:p-8 max-w-7xl mx-auto'>
+    <div className='bg-black min-h-screen p-4 sm:p-6 lg:p-8 overflow-y-auto max-h-screen'>
       {/* Header */}
       <div className='flex justify-between items-center mb-6'>
         <button className='w-12 h-12 bg-gray-800 rounded-full flex items-center justify-center'>
